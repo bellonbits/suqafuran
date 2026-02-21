e477a22a4608ca934e2c61a0d456c4b7241ce0f8
HarunMbaabu <mbaabuharun8@gmail.com>
2026-02-22 00:51:16 +0300

    feat: update Somali regions and cities with comprehensive data

--- src/utils/somaliRegions.ts
+++ src/utils/somaliRegions.ts
@@ -1,48 +1,67 @@
 export const SOMALI_REGIONS = [
     {
-        name: "Banadir",
-        cities: ["Mogadishu", "Hodan", "Warta Nabada", "Daynile", "Hamar Weyne"]
-    },
-    {
-        name: "Somaliland",
-        cities: ["Hargeisa", "Berbera", "Burao", "Gabiley", "Las Anod", "Erigavo"]
+        name: "Banaadir",
+        cities: [
+            "Mogadishu", "Afgooye", "Balcad", "Jazeera", "Lafoole", "Ceelasha Biyaha"
+        ]
     },
     {
         name: "Puntland",
-        cities: ["Garowe", "Bosaso", "Galkayo", "Qardho", "Badhan"]
+        cities: [
+            "Garowe", "Burtinle", "Dangorayo", "Eyl", "Godobjiraan", "Ceel Daahir",
+            "Bosaso", "Qandala", "Iskushuban", "Bargal", "Bandarbayla", "Caluula",
+            "Hafun", "Carmo", "Galkayo", "Galdogob", "Jariban", "Harfo", "Bacaadweyn",
+            "Qardho", "Xingalol", "Ufayn", "Rako", "Timirshe"
+        ]
     },
     {
-        name: "Jubaland",
-        cities: ["Kismayo", "Afmadow", "Bardhere", "Luuq"]
-    },
-    {
-        name: "South West",
-        cities: ["Baidoa", "Merca", "Barawe", "Wanlaweyn"]
-    },
-    {
-        name: "Hirshabelle",
-        cities: ["Jowhar", "Beledweyne", "Buloburde", "Balcad"]
+        name: "Somaliland",
+        cities: [
+            "Hargeisa", "Tog Wajaale", "Gabiley", "Baligubadle", "Arabsiyo", "Faraweyne",
+            "Berbera", "Sheikh", "Mandera", "Xiis (Heis)", "Burao", "Oodweyne", "Qoyta",
+            "Widhwidh", "Borama", "Baki", "Lughaya", "Zeila (Saylac)", "Dilla",
+            "Ceerigaabo", "Badhan", "Dhahar", "Maydh"
+        ]
     },
     {
         name: "Galmudug",
-        cities: ["Dhusamareb", "Adado", "Galkayo (South)", "Hobyo"]
+        cities: [
+            "Dhusamareeb", "Guriceel", "Balanbale", "Galinsoor", "Eldher", "Abudwaq",
+            "Galkayo (South)", "Gaalkacyo", "Bacaadweyne", "Hobyo", "Xarardheere", "Ceel Dheer"
+        ]
+    },
+    {
+        name: "Hirshabelle",
+        cities: [
+            "Jowhar", "Mahaday", "Balcad", "Raage Ceelle", "Beledweyne", "Bulo Burde",
+            "Jalalaqsi", "Mataban"
+        ]
+    },
+    {
+        name: "Jubaland",
+        cities: [
+            "Kismayo", "Jamaame", "Afmadow", "Dhobley", "Badhaadhe", "Bu'aale", "Bardhere"
+        ]
+    },
+    {
+        name: "SouthWest",
+        cities: [
+            "Baidoa", "Buur Hakaba", "Berdale", "Qansax Dheere", "Diinsoor", "Awdinle",
+            "Leego", "Goofgaduud", "Hudur (Xudur)", "Tiyeglow", "Wajid", "Ceel Barde",
+            "Rabdhure", "Yeed", "Marka", "Janaale", "Awdheegle", "Sablaale", "Kurtunwaarey",
+            "Bulo Mareer", "Golweyn", "Baraawe", "Kuumyo Baraawe", "Danow", "Dhaaytubako",
+            "Wanlaweyn", "Qoryoley", "Shalanbood"
+        ]
+    },
+    {
+        name: "North East",
+        cities: [
+            "Laascaanood", "Taleh", "Xudun", "Kalabaydh", "Adhicadeeye", "Buuhoodle"
+        ]
     }
 ];
 
-// Flattened list for simple dropdowns if needed
-export const ALL_LOCATIONS = [
-    "Mogadishu",
-    "Hargeisa",
-    "Bosaso",
-    "Kismayo",
-    "Baidoa",
-    "Garowe",
-    "Berbera",
-    "Beledweyne",
-    "Burao",
-    "Galkayo",
-    "Jowhar",
-    "Mandera",
-    "Wajir",
-    "Garissa"
-].sort();
+// Flattened list of unique locations for simple dropdowns if needed
+export const ALL_LOCATIONS = Array.from(
+    new Set(SOMALI_REGIONS.flatMap(region => region.cities))
+).sort();
