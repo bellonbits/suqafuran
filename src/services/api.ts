b444aadc96ed9d8ace8eeb9bdbc84af637c96b2f
HarunMbaabu <mbaabuharun8@gmail.com>
2026-03-14 13:35:30 +0300

    noww

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { useAuthStore } from '../store/useAuthStore';
 
-export const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://api.guri24.com:8888/api/v1';
+export const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://api.suqafuran.com:8888/api/v1';
 
 const api = axios.create({
     baseURL: API_BASE_URL,
