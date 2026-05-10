b8135131874746a722f1ba4934c67abde50a4f77
Your Name <you@example.com>
2026-05-11 01:09:27 +0300

    nowwww

--- backend/app/models/common.py
+++ backend/app/models/common.py
@@ -1,5 +1,11 @@
+from datetime import datetime
 from typing import Optional
-from sqlmodel import SQLModel
+from sqlmodel import SQLModel, Field
+
+
+class TimestampModel(SQLModel):
+    created_at: datetime = Field(default_factory=datetime.utcnow)
+    updated_at: datetime = Field(default_factory=datetime.utcnow)
 
 
 class OwnerRead(SQLModel):
